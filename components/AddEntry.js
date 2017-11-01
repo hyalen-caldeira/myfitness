e2ba418153f03496da55ea0d1e34019594bb3cd8
Hyalen Moreira <hyalen.moreira@gmail.com>
2017-10-31 18:10:29 -0700

    Add support for redux

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -16,7 +23,7 @@ function SubmitBtn ({ onPress }) {
   )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -24,7 +31,6 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
-  
   increment = (metric) => {
     const { max, step } = getMetricMetaInfo(metric)
 
@@ -37,7 +43,6 @@ export default class AddEntry extends Component {
       }
     })
   }
-
   decrement = (metric) => {
     this.setState((state) => {
       const count = state[metric] - getMetricMetaInfo(metric).step
@@ -48,38 +53,38 @@ export default class AddEntry extends Component {
       }
     })
   }
-
   slide = (metric, value) => {
     this.setState(() => ({
       [metric]: value
     }))
   }
-
   submit = () => {
     const key = timeToString()
     const entry = this.state
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: entry
+    }))
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry })
 
     // Clear local notification
   }
-
   reset = () => {
     const key = timeToString()
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: getDailyReminderValue()
+    }))
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key)
   }
-
   render() {
     const metaInfo = getMetricMetaInfo()
 
@@ -128,3 +133,15 @@ export default class AddEntry extends Component {
     )
   }
 }
+
+function mapStateToProps (state) {
+  const key = timeToString()
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(AddEntry)
